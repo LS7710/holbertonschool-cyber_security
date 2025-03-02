d2891958ef151cd20737117d4bc434c93cf88aab
LS7710 <luissoto7710>
2025-03-02 17:24:53 -0500

    task 9

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo iptables -P INPUT DROP && sudo iptables -A INPUT -p tcp --dport ssh -j ACCEPT
-sudo iptables -P FORWARD DROP && sudo iptables -P OUTPUT ACCEPT
+sudo iptables -A INPUT -j DROP && sudo iptables -P FORWARD DROP && sudo iptables -P OUTPUT ACCEPT
