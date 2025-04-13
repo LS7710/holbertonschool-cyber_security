3c8bc2448f0c9859a504371fb08770b19f3b3a1c
LS7710 <luissoto7710>
2025-04-13 18:16:12 -0400

    task 2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-tail -n 1000 auth.log | grep "Failed password" | awk '{print $(NF)}' | sort | uniq -c | sort -nr > .failed_users.txt
-tail -n 1000 auth.log | grep "Accepted password" | awk '{print $(NF)}' | sort | uniq -c | sort -nr > .successful_users.txt
+tail -n 1000 auth.log | grep "Failed password" | awk '{for(i=1;i<=NF;i++){if($i=="for"){print $(i+1)}}}' | sort | uniq -c | sort -nr > .failed_users.txt
+tail -n 1000 auth.log | grep "Accepted password" | awk '{for(i=1;i<=NF;i++){if($i=="for"){print $(i+1)}}}' | sort | uniq -c | sort -nr > .successful_users.txt
 
 cat .successful_users.txt | while read count user
 do
