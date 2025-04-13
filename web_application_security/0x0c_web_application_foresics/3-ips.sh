acad2e7bae6152f7ef4c182a3fb45c30aada27b4
LS7710 <luissoto7710>
2025-04-13 18:25:31 -0400

    task 3 fix

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail -n 1000 auth.log | grep "Accepted password for" | awk '{for(i=1;i<=NF;i++){if($i=="from"){print $(i+1)}}}' | sort | uniq | wc -l
+grep "Accepted password for" auth.log | grep -oP 'from \K[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort | uniq | wc -l
